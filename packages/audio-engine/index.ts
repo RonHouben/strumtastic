97ef06273c6f19e2419e9f43130767683e936443
Ron Houben <ron.houben85@gmail.com>
2022-12-23 17:42:23 +0100

    feat: added ml5 pitchDetection to audio-engine

--- packages/audio-engine/index.ts
+++ packages/audio-engine/index.ts
@@ -1,3 +1,5 @@
+import * as ml5 from 'ml5';
+
 export interface OscillatorOptions {
   type: OscillatorNode['type'];
   hertz: number;
@@ -8,14 +10,17 @@ export interface AudioEngineDebugOptions {
 }
 
 interface AudioEngineOptions {
-  inputAudioStream: MediaStream;
+  mediaStream: MediaStream;
 }
 
 export class AudioEngine {
   private readonly audioContext: AudioContext;
-  private readonly analyser: AnalyserNode;
-  private readonly inputAudioStream: MediaStream;
-  private readonly inputAudioStreamSource: MediaStreamAudioSourceNode;
+  private readonly analyserNode: AnalyserNode;
+  private readonly mediaStream: MediaStream;
+  private readonly mediaStreamSourceNode: MediaStreamAudioSourceNode;
+  private readonly pitchDetectionModelPath: string =
+    './machine-learning-models/crepe/';
+  private readonly pitchDetector: ml5.PitchDetector;
   private oscillator?: OscillatorNode;
   private requestAnimationFrameId?: number;
   private _currentFrequency = -1;
@@ -24,23 +29,33 @@ export class AudioEngine {
   public readonly bufferLength: number;
   public readonly frequencyData: Float32Array;
 
-  constructor({ inputAudioStream }: AudioEngineOptions) {
-    this.inputAudioStream = inputAudioStream;
+  constructor({ mediaStream }: AudioEngineOptions) {
+    this.mediaStream = mediaStream;
     this.audioContext = new AudioContext();
-    this.analyser = this.audioContext.createAnalyser();
 
-    this.analyser.fftSize = 2048;
-    this.analyser.minDecibels = -50;
-    this.analyser.maxDecibels = -10;
-    this.analyser.smoothingTimeConstant = 0.85;
+    this.analyserNode = this.audioContext.createAnalyser();
+    this.analyserNode.fftSize = 2048;
+    this.analyserNode.minDecibels = -50;
+    this.analyserNode.maxDecibels = -10;
+    this.analyserNode.smoothingTimeConstant = 0.85;
 
-    this.bufferLength = this.analyser.frequencyBinCount;
+    this.bufferLength = this.analyserNode.frequencyBinCount;
     this.frequencyData = new Float32Array(this.bufferLength);
 
     // connect inputAudioStreamSouce
-    this.inputAudioStreamSource = this.audioContext.createMediaStreamSource(
-      this.inputAudioStream
+    this.mediaStreamSourceNode = this.audioContext.createMediaStreamSource(
+      this.mediaStream
     );
+
+    this.pitchDetector = ml5.pitchDetection(
+      this.pitchDetectionModelPath,
+      this.audioContext,
+      this.mediaStream,
+      () => {
+        console.info('ml5 pitchDetection model has been loaded');
+      }
+    );
+
     // this.analyser.connect(this.audioContext.destination);
   }
 
@@ -52,89 +67,95 @@ export class AudioEngine {
     return this._isStreamingAudio;
   }
 
-  private streamInputAudio(): void {
+  private async streamInputAudio(): Promise<void> {
     this.requestAnimationFrameId = requestAnimationFrame(() =>
       this.streamInputAudio()
     );
 
-    this.analyser.getFloatTimeDomainData(this.frequencyData);
+    this.analyserNode.getFloatTimeDomainData(this.frequencyData);
 
-    this._currentFrequency = this.getFrequency(
-      this.frequencyData,
-      this.audioContext.sampleRate
-    );
+    // this._currentFrequency = this.getFrequency(
+    //   this.frequencyData,
+    //   this.audioContext.sampleRate
+    // );
+
+    this._currentFrequency = await this.getFrequency();
 
     this.setIsStreamingAudio(true);
   }
 
-  private getFrequency(buffer: Float32Array, sampleRate: number): number {
-    // source: https://github.com/cwilso/PitchDetect
-    // source: https://github.com/sablevsky/freelizer
+  private async getFrequency(): Promise<number> {
+    return this.pitchDetector.getPitch();
+  } 
 
-    // Not enough signal check
-    const RMS = Math.sqrt(
-      buffer.reduce((acc, el) => acc + el ** 2, 0) / buffer.length
-    );
-    if (RMS < 0.001) return NaN;
-    // console.log(RMS)
-    // if (RMS < 0.2) return NaN;
+  // private getFrequency(buffer: Float32Array, sampleRate: number): number {
+  //   // source: https://github.com/cwilso/PitchDetect
+  //   // source: https://github.com/sablevsky/freelizer
 
-    const THRES = 0.2;
-    let r1 = 0;
-    let r2 = buffer.length - 1;
-    for (let i = 0; i < buffer.length / 2; ++i) {
-      if (Math.abs(buffer[i]) < THRES) {
-        r1 = i;
-        break;
-      }
-    }
-    for (let i = 1; i < buffer.length / 2; ++i) {
-      if (Math.abs(buffer[buffer.length - i]) < THRES) {
-        r2 = buffer.length - i;
-        break;
-      }
-    }
+  //   // Not enough signal check
+  //   const RMS = Math.sqrt(
+  //     buffer.reduce((acc, el) => acc + el ** 2, 0) / buffer.length
+  //   );
+  //   if (RMS < 0.001) return NaN;
+  //   // console.log(RMS)
+  //   // if (RMS < 0.2) return NaN;
 
-    const buf2 = buffer.slice(r1, r2);
-    const c = new Array(buf2.length).fill(0);
-    for (let i = 0; i < buf2.length; ++i) {
-      for (let j = 0; j < buf2.length - i; ++j) {
-        c[i] = c[i] + buf2[j] * buf2[j + i];
-      }
-    }
+  //   const THRES = 0.2;
+  //   let r1 = 0;
+  //   let r2 = buffer.length - 1;
+  //   for (let i = 0; i < buffer.length / 2; ++i) {
+  //     if (Math.abs(buffer[i]) < THRES) {
+  //       r1 = i;
+  //       break;
+  //     }
+  //   }
+  //   for (let i = 1; i < buffer.length / 2; ++i) {
+  //     if (Math.abs(buffer[buffer.length - i]) < THRES) {
+  //       r2 = buffer.length - i;
+  //       break;
+  //     }
+  //   }
 
-    let d = 0;
-    for (; c[d] > c[d + 1]; ++d);
+  //   const buf2 = buffer.slice(r1, r2);
+  //   const c = new Array(buf2.length).fill(0);
+  //   for (let i = 0; i < buf2.length; ++i) {
+  //     for (let j = 0; j < buf2.length - i; ++j) {
+  //       c[i] = c[i] + buf2[j] * buf2[j + i];
+  //     }
+  //   }
 
-    let maxval = -1;
-    let maxpos = -1;
-    for (let i = d; i < buf2.length; ++i) {
-      if (c[i] > maxval) {
-        maxval = c[i];
-        maxpos = i;
-      }
-    }
-    let T0 = maxpos;
+  //   let d = 0;
+  //   for (; c[d] > c[d + 1]; ++d);
 
-    let x1 = c[T0 - 1];
-    let x2 = c[T0];
-    let x3 = c[T0 + 1];
-    let a = (x1 + x3 - 2 * x2) / 2;
-    let b = (x3 - x1) / 2;
+  //   let maxval = -1;
+  //   let maxpos = -1;
+  //   for (let i = d; i < buf2.length; ++i) {
+  //     if (c[i] > maxval) {
+  //       maxval = c[i];
+  //       maxpos = i;
+  //     }
+  //   }
+  //   let T0 = maxpos;
 
-    return sampleRate / (a ? T0 - b / (2 * a) : T0);
-  }
+  //   let x1 = c[T0 - 1];
+  //   let x2 = c[T0];
+  //   let x3 = c[T0 + 1];
+  //   let a = (x1 + x3 - 2 * x2) / 2;
+  //   let b = (x3 - x1) / 2;
+
+  //   return sampleRate / (a ? T0 - b / (2 * a) : T0);
+  // }
 
   private setIsStreamingAudio(isStreamingAudio: boolean): void {
     this._isStreamingAudio = isStreamingAudio;
   }
 
   public startInputAudioStream(): void {
-    this.inputAudioStreamSource.connect(this.analyser);
+    this.mediaStreamSourceNode.connect(this.analyserNode);
 
     // this is for debug purposes
     if (this.oscillator && this.oscillator?.context.state !== 'running') {
-      this.oscillator.connect(this.analyser);
+      this.oscillator.connect(this.analyserNode);
       this.oscillator.start();
     }
 
