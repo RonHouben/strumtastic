a3909501d5608459c1d26e9a4816d0de598b723f
Ron Houben <ron.houben85@gmail.com>
2023-01-14 18:00:23 +0100

    styling: made side menu smaller

--- packages/ui/components/Sidebar.tsx
+++ packages/ui/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import NavMenuButtons from "./NavMenuButtons";
 
 export default function Sidebar() {
   return (
-    <div className="col-span-3 col-start-1 max-sm:hidden">
+    <div className="col-span-2 col-start-1 max-sm:hidden">
       <NavMenuButtons />
     </div>
   );
