9cdaec81ac413a82dc95a88de3c0811e882b1de8
Ron Houben <ron.houben85@gmail.com>
2023-01-15 10:39:22 +0100

    fix: added key prop to Skeleton

--- packages/ui/components/Skeleton.tsx
+++ packages/ui/components/Skeleton.tsx
@@ -9,7 +9,7 @@ export default function Skeleton({ lines }: Props) {
         ...new Array(lines)
           .fill(1)
           .map((_, i) => (
-            <span className="inline-block h-5 w-10/12 animate-pulse rounded-md bg-secondary-900" />
+            <span key={i} className="inline-block h-5 w-10/12 animate-pulse rounded-md bg-secondary-900" />
           ))
       ]}
     </>
