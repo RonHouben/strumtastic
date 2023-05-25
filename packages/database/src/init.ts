a0bf8edd7abd7e2327bf70a7d69be5213f6bb3c6
Ron Houben <r.houben@ksyos.nl>
2023-05-25 17:05:33 +0200

    chore: removed debug and dotenv

--- packages/database/src/init.ts
+++ packages/database/src/init.ts
@@ -1,20 +1,8 @@
-import * as dotenv from 'dotenv';
 import admin from 'firebase-admin';
 import * as fireorm from 'fireorm';
 
-dotenv.config();
-
 export function initFirestore() {
   console.log('initializing firestore');
-
-  console.log({
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientMailNext: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-    privateKeyNext: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
-    projectIdNext: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  })
 
   if (admin.apps.length === 0) {
     admin.initializeApp({
