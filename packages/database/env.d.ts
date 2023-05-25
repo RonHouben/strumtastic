254cff303311151731b14b56c7226c3d1f15f2a9
Ron Houben <r.houben@ksyos.nl>
2023-05-25 16:45:45 +0200

    fix: this should correct hte database env variabels

--- packages/database/env.d.ts
+++ packages/database/env.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-		NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL: string;
-		NEXT_PUBLIC_FIREBASE_PRIVATE_KEY: string;
-		NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+		FIREBASE_CLIENT_EMAIL: string;
+		FIREBASE_PRIVATE_KEY: string;
+		FIREBASE_PROJECT_ID: string;
   }
 }
--- packages/database/src/init.ts
+++ packages/database/src/init.ts
@@ -8,13 +8,13 @@ export function initFirestore() {
   console.log('initializing firestore');
 
   if (admin.apps.length === 0) {
-    const app = admin.initializeApp({
+    admin.initializeApp({
       credential: admin.credential.cert({
-        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        projectId: process.env.FIREBASE_PROJECT_ID
       }),
-      databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
+      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
     });
 
     const firestore = admin.firestore();
