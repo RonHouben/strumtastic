5a7648af6ef5be74b6f5340ab0a8e4180a4a569e
Ron Houben <r.houben@ksyos.nl>
2023-05-25 16:54:13 +0200

    debug: added debugging console.log

--- packages/database/src/init.ts
+++ packages/database/src/init.ts
@@ -7,6 +7,15 @@ dotenv.config();
 export function initFirestore() {
   console.log('initializing firestore');
 
+  console.log({
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientMailNext: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+    privateKeyNext: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+    projectIdNext: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  })
+
   if (admin.apps.length === 0) {
     admin.initializeApp({
       credential: admin.credential.cert({
