f0528be8187b0d1e349b664b57d6b4748baa9b8a
RonHouben <ron.houben85@gmail.com>
2023-06-13 22:07:20 +0200

    feat: added vercel analytics

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,10 +1,16 @@
 import { ReactNode } from 'react';
 import { GlobalStateProvider } from 'ui/providers/GlobalStateProvider';
+import { Analytics } from '@vercel/analytics/react';
 
 interface Props {
   children: ReactNode;
 }
 
 export function Providers({ children }: Props) {
-  return <GlobalStateProvider>{children}</GlobalStateProvider>;
+  return (
+    <>
+      <GlobalStateProvider>{children}</GlobalStateProvider>
+      <Analytics />
+    </>
+  );
 }
