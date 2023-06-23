648611be098dc80a62f7b68c493cbb4cd8ac2bcd
RonHouben <ron.houben85@gmail.com>
2023-06-23 12:33:52 +0200

    feat: added sslmode=require to postgres connectionString

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,7 +1,9 @@
-import type { Config } from "drizzle-kit";
- 
+import type { Config } from 'drizzle-kit';
+
 export default {
-  schema: "./src/schemas",
-  out: "./drizzle",
-	connectionString: process.env.POSTGRES_URL,
+  schema: './src/schemas',
+  out: './drizzle',
+  dbCredentials: {
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  }
 } satisfies Config;
