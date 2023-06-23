761b0cb1a5331bfd8c97c590dd774660f6a11de9
RonHouben <ron.houben85@gmail.com>
2023-06-23 15:59:22 +0200

    fix: ran `git config core.ignorecase false` to fix Vercel deployment issue

---
+++ packages/ui/components/label.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "ui/utils"
+
+const labelVariants = cva(
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+)
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelVariants> & { required?: boolean }
+>(({ className, required, ...props }, ref) => (
+  <>
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelVariants(), className)}
+    {...props}
+  />
+  {required && (<span className='text-sm text-red-500'>*</span>)}
+  </>
+))
+Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label }
